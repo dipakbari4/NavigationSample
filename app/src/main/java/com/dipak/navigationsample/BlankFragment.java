fa1270d501d2fb56cfbdf70604edceda3ee9a53a
Dipak Bari <dipakbari4@gmail.com>
2022-08-28 13:14:55 +0530

    [Update] BlankFragment bavController implementation

--- app/src/main/java/com/dipak/navigationsample/BlankFragment.java
+++ app/src/main/java/com/dipak/navigationsample/BlankFragment.java
@@ -2,11 +2,17 @@ package com.dipak.navigationsample;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.dipak.navigationsample.databinding.FragmentBlankBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +20,9 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class BlankFragment extends Fragment {
+
+    private FragmentBlankBinding binding;
+    private NavController navController;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -56,9 +65,14 @@ public class BlankFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        binding = FragmentBlankBinding.inflate(inflater, container, false);
+        navController = NavHostFragment.findNavController(this);
+        binding.btnAnother.setOnClickListener(view -> {
+            navController.navigate(R.id.blankFragment2);
+        });
+        return binding.getRoot();
     }
 }
