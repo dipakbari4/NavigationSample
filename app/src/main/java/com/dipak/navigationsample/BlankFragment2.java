1d779708121a1e649d8d4a7d31e4855ed31a41ee
Dipak Bari <dipakbari4@gmail.com>
2022-08-28 13:16:44 +0530

    [Update] BlankFragment2 implementation NavCtrl on back pressed

--- app/src/main/java/com/dipak/navigationsample/BlankFragment2.java
+++ app/src/main/java/com/dipak/navigationsample/BlankFragment2.java
@@ -1,12 +1,17 @@
 package com.dipak.navigationsample;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.dipak.navigationsample.databinding.FragmentBlank2Binding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +20,8 @@ import android.view.ViewGroup;
  */
 public class BlankFragment2 extends Fragment {
 
+    private NavController navController;
+    private FragmentBlank2Binding binding;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -56,9 +63,12 @@ public class BlankFragment2 extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank2, container, false);
+        navController = NavHostFragment.findNavController(this);
+        binding = FragmentBlank2Binding.inflate(inflater, container, false);
+        binding.backButton.setOnClickListener(view -> navController.popBackStack());
+        return binding.getRoot();
     }
 }
